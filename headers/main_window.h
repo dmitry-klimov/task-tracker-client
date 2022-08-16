d5cd159c6ad8c36dd5e07ea77eac1abde818253f
dmitryklimov <taurusd@yandex.ru>
2022-08-17 00:31:49 +0300

    MainWindow class has been added

---
+++ headers/main_window.h
@@ -0,0 +1,11 @@
+#ifndef STCTESTTASLCLIENT_MAIN_WINDOW_H
+#define STCTESTTASLCLIENT_MAIN_WINDOW_H
+
+#include <QMainWindow>
+
+class MainWindow : public QMainWindow {
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow() override;
+};
+#endif //STCTESTTASLCLIENT_MAIN_WINDOW_H
---
+++ src/main_window.cpp
@@ -0,0 +1,11 @@
+#include "headers/main_window.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+{
+
+}
+
+MainWindow::~MainWindow(){
+
+}
